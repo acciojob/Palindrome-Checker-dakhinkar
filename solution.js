1b58edbdb7f40d76621cb2e58f7d7659da9fe641
Pranit Dakhinkar <97027846+dakhinkar@users.noreply.github.com>
2022-12-01 20:17:16 +0530

    By using Built in method

--- solution.js
+++ solution.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+//   By using for loop
+//      str = str.toLowerCase();
+//         let len = str.length;
+//         for(let i = 0; i< len; i++){
+//                 if(str[i] !== str[len -1 - i]){
+//                         return false;
+//                 }
+//         }
+//         return true;
+  
+// Built in method  
+    let newStr = str.toLowerCase().split("").reverse().join("");
+        return newStr == str.toLowerCase();
 }
 module.exports = palindrome
